82b8088490e15507d634c44bcd37ac9de4523f60
JOHN ANDREI <andriegalutera@gmail.com>
2025-12-28 10:33:26 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="SuiKernel"
+KERNEL_NAME="GrayRavens-Hyperion-V4"
 # Kernel Build variables
-USER="KanagawaYamada"
-HOST="HoshimachiSuisei"
+USER="Andriana"
+HOST="GrayRavensTeam"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
 ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
 ANYKERNEL_BRANCH="gki"
 # Kernel Source
-KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
-KERNEL_BRANCH="suikernel-main"
+KERNEL_REPO="https://github.com/XTENSEI/android_kernel_common-5.10"
+KERNEL_BRANCH="android12-5.10"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO="https://github.com/XTENSEI/GrayRavens-GKI/releases"
 # Clang
 CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
 CLANG_BRANCH="lineage-20.0"
